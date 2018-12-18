77b1134fe33cf75b66209bdb94844bb4d4d34521
srvander <srvanderplas@gmail.com>
2018-12-18 09:11:42 -0600

    Create metadata tables

---
+++ Code/Create_Metadata_Tables.R
@@ -0,0 +1,38 @@
+library(openxlsx)
+library(tidyverse)
+library(here)
+
+shoeprint_data <- read.xlsx(here("shoeprints data.xlsx"), check.names = F, detectDates = T)
+
+shoe_models <- shoeprint_data %>%
+  mutate(Model = ifelse(Available.option == 1, Option.1.model.and.color, Option.2.model.and.color),
+         Size = ifelse(Available.option == 1, Option.1.size, Option.2.size)) %>%
+  select(ID, Cohort, Model, Size, Gender = `Tech..Men/Women`) %>%
+  mutate(Brand = str_extract(Model, "Nike|Adidas"),
+         Size = sprintf("%s %s", Size, Gender),
+         Color = str_remove_all(Model, "((?:Nike|Adidas|.Men.|.Women.) )")) %>%
+  select(-Model, -Gender) %>%
+  mutate(ID = sprintf("%03d", as.numeric(ID)),
+         Model = ifelse(Brand == "Nike", "Winflo 4", "Seeley")) %>%
+  select(ShoeID = ID, Cohort, Brand, Model, Size, Color)
+
+
+participant_data <- read.xlsx(here("PII/shoeprints_key.xlsx"), check.names = F, detectDates = T)
+
+participants <- participant_data %>%
+  mutate(WearerID = sapply(email, digest::digest) %>% factor(levels = unique(.)) %>% as.numeric()) %>%
+  select(WearerID, ShoeID = ID, Weight, Height,
+         Do.you.engage.in.any.of.the.activities.listed.bel:`As.a.study.participant,.how.often.do.you.anticipa`)
+
+
+survey_data <- read.xlsx(here("Surveys/Organized surveys (Full and Clean).xlsx"),
+                         check.names = T, detectDates = T, rows = 1:456) %>%
+  mutate(ShoeID = sprintf("%03d", as.numeric(Participant.ID)),
+         Timestamp = convertToDateTime(Timestamp)) %>%
+  select(-starts_with("Participant.ID")) %>%
+  select(32, 1:31) %>%
+  set_names(str_replace(names(.), "Estimated.proportion.of.time.Shoes.were.worn.on.(.*)\\.\\.\\.Chart\\..\\.", "\\1_Clean")) %>%
+  set_names(str_replace(names(.), "Estimated.proportion.of.time.shoes.were.worn.while.participating.in.(.*).since.your.last.visit.*", "\\1_Clean")) %>%
+  set_names(c(names(.)[1:5], "Number.of.steps_Range1000", "Notes", "Hours.worn.per.week", "Hours.worn.per.week_Range10", "Percent.time.in.study.shoes.of.total.time.wearing.shoes", names(.)[11:32])) %>%
+  set_names(tools::toTitleCase(names(.))) %>%
+  set_names(str_replace(names(.), "\\.{1,}", "."))
